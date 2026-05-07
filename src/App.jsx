6577e1fe47a63ff1a80ec209e02062439496b1de
Dana <dana_her.za@hotmail.com>
2026-05-07 13:59:06 -0600

    commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Button from './Web components/button'
+
+function App() {
+  return (
+    <div>
+      <Button />
+      <Button />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Web components/button.jsx
@@ -0,0 +1,11 @@
+import './Button.css'
+
+function Button() {
+  return (
+    <button className="btn">
+      Comprar
+    </button>
+  )
+}
+
+export default Button
